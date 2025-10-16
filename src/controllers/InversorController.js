b200ea74f3fa596d1ed1c9680a7d93315945dbc7
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-10-16 01:13:51 -0300

    Inversor de controle
    Inversor de controle

--- src/controllers/InversorController.js
+++ src/controllers/InversorController.js
@@ -2,3 +2,40 @@ const InversorService = require('../models/InversorService');
 const LogRepository = require('../models/LogRepository');
 const fs = require('fs');
 const path = require('path');
+
+// Rota GET /log: Lidar com injeção de dados sem Template Engine
+class InversorController {
+    static renderLog(req, res) {
+        // O Controller solicita os dados ao Model (LogRepository)
+        const historico = LogRepository.getLog();
+
+        // Monta o código HTML para injetar
+        let logHtml = '<ul>';
+        historico.forEach(item => {
+            logHtml += `<li>Original: ${item.original} | Invertida: **${item.invertida}** | Data: ${item.data}</li>`;
+        });
+        logHtml += '</ul>';
+
+        // Lê o arquivo HTML da View
+        const logFilePath = path.join(__dirname, '..', 'views', 'log.html');
+        fs.readFile(logFilePath, 'utf8', (err, data) => {
+            if (err) {
+                console.error("Erro ao ler log.html:", err);
+                return res.status(500).send("Erro interno do servidor.");
+            }
+            
+            // Substitui a tag de placeholder (simulando injeção)
+            const htmlModificado = data.replace('<div id="historico-container"></div>', 
+                                                `<div id="historico-container">${logHtml}</div>`);
+            
+            // Envia o HTML modificado ao usuário
+            res.send(htmlModificado);
+        });
+    }
+
+    // Rotas de Inversão (POST /inverter e GET /resultado) virão a seguir...
+    static processarInversao(req, res) { /* ... */ } 
+    static renderResultado(req, res) { /* ... */ } 
+}
+
+module.exports = InversorController;
